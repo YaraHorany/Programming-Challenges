cae4ab2c1f50e218b15b4af0305aeb532f2b6471
Yara Horany <yara.horany1@gmail.com>
2022-11-27 17:01:48 +0200

    Permutations

--- Permutations.py
+++ Permutations.py
@@ -23,3 +23,5 @@ def permute(nums):
 
 
 print(permute([1,2,3])) # Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+print(permute([0,1])) # Output: [[0,1],[1,0]]
+print(permute([1])) # Output: [[1]]
